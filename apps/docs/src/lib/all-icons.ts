e48a2597f58abdb8ba698aaee5d4932e209e2116
BryanBerger98 <contact+github@bryanberger.dev>
2026-05-19 19:46:10 +0200

    feat(icons): add proton pass

--- apps/docs/src/lib/all-icons.ts
+++ apps/docs/src/lib/all-icons.ts
@@ -57,6 +57,7 @@ export const latestIconBySlug: Readonly<Record<string, BrandIconComponent>> = {
   netflix: BrandIcons.NetflixLatestIcon,
   notion: BrandIcons.NotionLatestIcon,
   openai: BrandIcons.OpenAILatestIcon,
+  'proton-pass': BrandIcons.ProtonPassLatestIcon,
   safari: BrandIcons.SafariLatestIcon,
   snapchat: BrandIcons.SnapchatLatestIcon,
   sora: BrandIcons.SoraLatestIcon,
